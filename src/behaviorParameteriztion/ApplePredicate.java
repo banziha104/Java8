e7b391e64eed01893d782b47d1f6f5c2242e2d39
iyeongjun <they6687@naver.com>
2017-12-26 05:41:38 +0900

    Add Lambda

--- src/behaviorParameteriztion/ApplePredicate.java
+++ src/behaviorParameteriztion/ApplePredicate.java
@@ -1,6 +1,8 @@
 package behaviorParameteriztion;
 
 import behaviorParameteriztion.behaviorParameterization.Apple;
+
+@FunctionalInterface
 public interface ApplePredicate {
     boolean test (Apple apple);
 }
